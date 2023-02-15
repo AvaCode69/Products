ac9b8d6209ff3d2309961676d2db6c9b7766c9ff
AvaCode69 <reacthmd34@gmail.com>
2023-02-15 11:56:51 +0100

    add loading and error features

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('products starter');
+console.log("products starter");
+const url = "https://course-api.com/javascript-store-productss";
+
+const productDom = document.querySelector(".products-center");
+
+const fetchProducts = async () => {
+  productDom.innerHTML = `<div class="loading"></div>
+`;
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    console.log(data);
+  } catch {
+    productDom.innerHTML = `<p class="error">There was an Error </p>`;
+  }
+};
+
+fetchProducts();
